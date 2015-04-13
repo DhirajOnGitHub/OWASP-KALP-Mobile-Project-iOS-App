e9ed7af938020672a4d3cb7c3a9021dfe0373a84
DhirajOnGitHub <dhiraj@varutra.com>
2015-04-13 20:42:57 +0530

    Revert "OWASP KALP Mobile Project iOS App Source Code"
    This reverts commit 5095984d5dc55cbab3a46008d575ca70536365aa.

--- OWASPKALP/platforms/ios/www/js/hyperlink.js
+++
@@ -1,6 +0,0 @@
-function loadURL(url)
-{
-    var ref =  window.open(encodeURI(url),'_system','location=yes');
-    //navigator.app.loadUrl(url, { openExternal:true });
-		return false;
-}
--- OWASPKALP/www/js/initialize.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-	
-}
